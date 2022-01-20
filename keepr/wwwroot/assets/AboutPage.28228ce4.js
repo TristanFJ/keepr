782982d9f5403a3be03d6d5794270612b3a733aa
Tristan Johns <tfj@protonmail.com>
2022-01-20 10:28:26 -0700

    npm run build

---
+++ keepr/wwwroot/assets/AboutPage.28228ce4.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.d2f3ff0e.js";import{o,b as t,d as s}from"./vendor.9cbc420e.js";const a={name:"AboutPage"},c={class:"about"},n=s("h1",null,"This is the about page",-1),r=[n];function _(d,i,f,p,u,h){return o(),t("div",c,r)}var b=e(a,[["render",_]]);export{b as default};
